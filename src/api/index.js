a2ca47c47fde527ee68ef8b06cdb9c4efcdff9c8
Jonathan Williams <31247452+Jonathan4github@users.noreply.github.com>
2021-07-22 07:59:23 +0100

    make card grid reusable for the three cards

--- src/api/index.js
+++ src/api/index.js
@@ -9,39 +9,25 @@ export const fetchCovidTrackRecord = async (country) => {
     changeableUrl = `${url}/countries/${country}`;
   }
 
-  try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+  const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
-    return { confirmed, recovered, deaths, lastUpdate };
-  } catch (error) {
-    console.log(error);
-  }
+  return { confirmed, recovered, deaths, lastUpdate };
 }
 
 export const fetchDailyData = async () => {
-  try {
-    const { data } = await axios.get(`${url}/daily`);
+  const { data } = await axios.get(`${url}/daily`);
 
-    const modifiedData = data.map((dailyData) => ({
-      confirmed: dailyData.confirmed.total,
-      deaths: dailyData.deaths.total,
-      date: dailyData.reportDate,
-    }));
+  const modifiedData = data.map((dailyData) => ({
+    confirmed: dailyData.confirmed.total,
+    deaths: dailyData.deaths.total,
+    date: dailyData.reportDate,
+  }));
 
-    return modifiedData;
-
-  } catch (error) {
-    console.log(error);
-  }
+  return modifiedData;
 }
 
 export const fetchCountries = async () => {
-  try {
-    const {data: {countries}} = await axios.get(`${url}/countries`);
+  const {data: {countries}} = await axios.get(`${url}/countries`);
 
-    return countries.map(({ name })=> (name));
-  } catch (error) {
-    console.log(error);
-  }
-
+  return countries.map(({ name })=> (name));
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,6 +6,29 @@ import styles from './Cards.module.css';
 
 const Cards = (props) => {
   const {data: {confirmed, recovered, deaths, lastUpdate} } = props;
+  const cardData = [
+    {
+      style: 'infected',
+      title: 'Infected',
+      value: confirmed && confirmed.value,
+      lastUpdate,
+      text: 'Number of active cases of COVID 19'
+    },
+    {
+      style: 'recovered',
+      title: 'Recovered',
+      value: recovered && recovered.value,
+      lastUpdate,
+      text: 'Number of recoveries from COVID 19'
+    },
+    {
+      style: 'deaths',
+      title: 'Deaths',
+      value: deaths && deaths.value,
+      lastUpdate,
+      text: 'Number of deaths caused by COVID 19'
+    }
+  ]
 
   if (!confirmed) {
     return <h1>Loading...</h1>
@@ -14,38 +37,20 @@ const Cards = (props) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justifyContent="center">
-        <Grid item component={Card} xs={12} md={3} className={className(styles.card, styles.infected)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Infected</Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={confirmed.value} duration={2.5} seperator="," />
-            </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of active cases of COVID 19</Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={className(styles.card, styles.recovered)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-            <Typography variant="body2">Number of recoveries from COVID 19</Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={className(styles.card, styles.deaths)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-            <Typography variant="h5">
-              <CountUp start={0} end={deaths.value} separator="," duration={2.5} />
-            </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of deaths caused by COVID 19</Typography>
-          </CardContent>
-        </Grid>
+        {
+          cardData.map(({style, title, value, lastUpdate, text }) => (
+            <Grid item component={Card} xs={12} md={3} className={className(styles.card, styles[style])}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>{title}</Typography>
+                <Typography variant="h5">
+                  <CountUp start={0} end={value} duration={2.5} seperator="," />
+                </Typography>
+                <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                <Typography variant="body2">{text}</Typography>
+              </CardContent>
+            </Grid>
+          ))
+        }
       </Grid>
     </div>
   )
